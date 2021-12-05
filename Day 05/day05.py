1e8318c18561c587a77a742afe3c57b2ad1bee95
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-05 19:15:16 +0300

    Part 2 of day 05 in Python is complete

--- Day 05/day05.py
+++ Day 05/day05.py
@@ -13,15 +13,18 @@ with open(fileName, 'r') as file:
         (x1, y1) = [int(n) for n in coord1.split(',')]
         (x2, y2) = [int(n) for n in coord2.split(',')]
 
-        if x1 != x2 and y1 != y2:
-            continue
-
         if (x1 == x2):
            for y in range(min(y1, y2), max(y1, y2)+1):
                seaMap[y][x1]+= 1 
-        else:
+        elif (y1 == y2):
            for x in range(min(x1, x2), max(x1, x2)+1):
                seaMap[y1][x]+= 1 
+        else:
+            ln = abs(x2-x1)
+            xSign = int((x2-x1)/abs(x2-x1))
+            ySign = int((y2-y1)/abs(y2-y1))
+            for i in range(0, ln+1):
+                seaMap[y1+i*ySign][x1+i*xSign] += 1
 
 mostDangerous = 0
 for y in range(0, len(seaMap)):
