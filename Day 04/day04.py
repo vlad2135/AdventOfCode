000e619006acdb62f01972dfdb648de032248dda
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-05 15:33:54 +0300

    Part 1 of Day 04 solved in Python

---
+++ Day 04/day04.py
@@ -0,0 +1,78 @@
+import sys
+import os
+
+os.chdir(sys.path[0])
+fileName = 'input'
+
+winNums = list()
+boards = list()
+with open(fileName, 'r') as file:
+    winNums = [ int(s) for s in file.readline().split(',')]
+
+    while True:
+        space = file.readline()
+        board = list()
+
+        for i in range(0, 5):
+            line = file.readline()
+            if line == '':
+                break
+            board.append([(int(s), False) for s in line.split()])
+
+        if len(board) < 1:
+            break
+
+        boards.append(board)
+
+
+def markNumber(board: list, num: int):
+    for row in range(0, len(board)):
+        for column in range(0, len(board[row])):
+            if board[row][column][0] == num:
+                board[row][column] = (num, True)
+
+
+def isWinnerBoard(board: list) -> bool:
+    for row in range(0, len(board)):
+        isWinner = True
+        for column in range(0, len(board[row])):
+            if board[row][column][1] == False:
+                isWinner = False
+        if isWinner:
+            return isWinner
+
+    for column in range(0, len(board[0])):
+        isWinner = True
+        for row in range(0, len(board)):
+            if board[row][column][1] == False:
+                isWinner = False
+        if isWinner:
+            return isWinner
+
+    return False
+
+
+def calcWinScore(board: list, winNumber: int) -> int:
+    sum = 0
+    for row in range(0, len(board)):
+        for column in range(0, len(board[row])):
+            if board[row][column][1] == False:
+                sum += board[row][column][0]
+
+    return sum * winNumber
+
+
+for i in range(0, 4):
+    retrievedNumber = winNums[i]
+    for b in boards:
+        markNumber(b, retrievedNumber)
+
+for i in range(4, len(winNums)):
+    retrievedNumber = winNums[i]
+    for b in boards:
+        markNumber(b, retrievedNumber)
+
+    winningBoard = list(filter(lambda b: isWinnerBoard(b), boards))
+    if len(winningBoard) > 0:
+        print(calcWinScore(winningBoard[0], retrievedNumber))
+        sys.exit()
