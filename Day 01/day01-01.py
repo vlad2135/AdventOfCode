12fcac3b0764feb271da1d5a7ef1e6fd18e11773
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-05 11:20:41 +0300

    01-01 python solution + input and test input data

--- Day 01/day01-01.py
+++ Day 01/day01-01.py
@@ -1,1 +1,20 @@
-print("Hello, world")
+import sys
+import os
+
+os.chdir(sys.path[0])
+
+increaseCnt = 0
+prev = None
+with open('input', 'r') as file:
+        prev = int(file.readline())
+        for line in file:
+            curr = int(line)
+            if curr > prev:
+                increaseCnt += 1
+            prev = curr
+
+print(increaseCnt)
+            
+
+
+
