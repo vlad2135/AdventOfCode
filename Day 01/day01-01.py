11c9f385c101f0d626e1e7c191e3e03bf0e5fd1f
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-05 11:39:54 +0300

    a bit clearer solution with itertools (external because of 'old' python)
    also venv

--- Day 01/day01-01.py
+++ Day 01/day01-01.py
@@ -1,17 +1,15 @@
 import sys
 import os
+import more_itertools
 
 os.chdir(sys.path[0])
 
 increaseCnt = 0
-prev = None
 with open('input', 'r') as file:
-        prev = int(file.readline())
-        for line in file:
-            curr = int(line)
-            if curr > prev:
+        for pair in more_itertools.pairwise(file):
+            print(pair)
+            if int(pair[1]) > int(pair[0]):
                 increaseCnt += 1
-            prev = curr
 
 print(increaseCnt)
             
