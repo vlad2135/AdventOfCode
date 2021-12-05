38c7078dce6f97edb6a6d3c5202eedfd6f6d8462
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-05 14:11:35 +0300

    WRONG attempt on part 2 of day 03

--- Day 03/day03.py
+++ Day 03/day03.py
@@ -2,13 +2,16 @@ import sys
 import os
 
 os.chdir(sys.path[0])
-fileName = 'input'
+fileName = 'input_tst'
 
-bitCount = [[0 for x in range(2)] for y in range(12)]
+#bitCount = [[0 for x in range(2)] for y in range(12)]
+bitCount = [[0 for x in range(2)] for y in range(5)]
+numbers = list()
 with open(fileName, 'r') as file:
     for line in file:
         digits = list(line)
-        for i in range(0,len(digits)-1):
+        numbers.append(line)
+        for i in range(0, len(digits)-1):
             bitCount[i][int(digits[i])] += 1
 
 gammaRateStr = ''
@@ -26,5 +29,33 @@ for pair in bitCount:
 gammaRate = int(gammaRateStr, 2)
 epsilonRate = int(epsilonRateStr, 2)
 print('Power consumption = ', gammaRate * epsilonRate)
+
+
+def filterByPosValue(s: str, pos: int, val: str) -> bool:
+    return s[pos] == val
+
+
+oxyRatingList = numbers.copy()
+co2RatingList = numbers.copy()
+
+posInNum = 0
+while len(oxyRatingList) > 1 or len(co2RatingList) > 1:
+    tmpOxy = oxyRatingList.copy()
+    tmpCo2 = co2RatingList.copy()
+    if (len(oxyRatingList)) > 1:
+        oxyRatingList = list(filter(lambda s: filterByPosValue(s, posInNum, gammaRateStr[posInNum]), oxyRatingList))
+
+    if (len(co2RatingList)) > 1:
+        co2RatingList = list(filter(lambda s: filterByPosValue(s, posInNum, epsilonRateStr[posInNum]), co2RatingList))
+    
+    if (len(oxyRatingList)) < 1:
+        breakpoint()
+
+    if (len(co2RatingList)) < 1:
+        breakpoint()
+
+    posInNum += 1
+
+print('Life support rating = ', int(oxyRatingList[0], 2) * int(co2RatingList[0], 2))
 
 
