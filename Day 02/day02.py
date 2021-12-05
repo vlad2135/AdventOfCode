7a5bcfc8a42c9587e54497b29da6b9bea32d4751
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-05 12:06:55 +0300

    day 02 part 01 correct answer

--- Day 02/day02.py
+++ Day 02/day02.py
@@ -4,5 +4,19 @@ import os
 os.chdir(sys.path[0])
 fileName = 'input'
 
+pos = 0
+depth = 0
 with open(fileName, 'r') as file:
-    pass
+    for line in file:
+        cmdPair = line.split()
+        cmd = cmdPair[0]
+        cmdVal = int(cmdPair[1])
+        # can't use match because of my Python being  < 3.10
+        if cmd == 'forward':
+            pos += cmdVal
+        elif cmd == 'down':
+            depth += cmdVal
+        elif cmd == 'up':
+            depth -= cmdVal
+
+print(pos * depth)
