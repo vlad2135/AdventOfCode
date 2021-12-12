021a49d7dcf8a8280f93af1753ba5237ee738fb2
Vladimir Bukreev <vlad2135@yandex.ru>
2021-12-12 22:02:43 +0300

    non working optimization for Part 2 of Day 06

--- Day 06/day06.py
+++ Day 06/day06.py
@@ -8,19 +8,26 @@ fishTimers = []
 with open(fileName, 'r') as file:
     fishTimers = [int(c) for c in file.readline().split(',')]
 
-dayCnt = 80
+dayCnt = 256
+fishTimersLen = len(fishTimers)
 
 while dayCnt > 0:
     newFishCnt = 0
-    for i in range(0, len(fishTimers)):
+    for i in range(0, fishTimersLen):
         fishTimers[i] = fishTimers[i] - 1
         if (fishTimers[i]) < 0:
             fishTimers[i] = 6
             newFishCnt += 1
 
     if newFishCnt > 0:
-        fishTimers.extend([8] * newFishCnt)
+        fishTimersLen += newFishCnt
+        if fishTimersLen > len(fishTimers):
+            fishTimers.extend([8] * len(fishTimers))
 
-    dayCnt-=1
+    dayCnt -= 1
+    print(dayCnt)
 
-print(len(fishTimers))
+print()
+# print(len(fishTimers))
+print(fishTimersLen)
+
