1f624302b8e49609f180136e65992e8afd57c545
Vladimir Bukreev <vlad2135@yandex.ru>
2021-12-12 22:16:44 +0300

    trying to compact 4 fishes into single int

--- Day 06/day06.py
+++ Day 06/day06.py
@@ -4,30 +4,29 @@ import os
 os.chdir(sys.path[0])
 fileName = 'input'
 
-fishTimers = []
+fishTimersInit = []
 with open(fileName, 'r') as file:
-    fishTimers = [int(c) for c in file.readline().split(',')]
+    fishTimersInit = [int(c) for c in file.readline().split(',')]
 
 dayCnt = 256
-fishTimersLen = len(fishTimers)
+fishTimersCnt = len(fishTimersInit)
 
 while dayCnt > 0:
     newFishCnt = 0
-    for i in range(0, fishTimersLen):
+    for i in range(0, fishTimersCnt):
         fishTimers[i] = fishTimers[i] - 1
         if (fishTimers[i]) < 0:
             fishTimers[i] = 6
             newFishCnt += 1
 
     if newFishCnt > 0:
-        fishTimersLen += newFishCnt
-        if fishTimersLen > len(fishTimers):
-            fishTimers.extend([8] * len(fishTimers))
+        fishTimers.extend([8] * newFishCnt)
+        fishTimersCnt += newFishCnt
 
     dayCnt -= 1
     print(dayCnt)
+    print(fishTimersCnt)
 
 print()
-# print(len(fishTimers))
-print(fishTimersLen)
+print(fishTimersCnt)
 
