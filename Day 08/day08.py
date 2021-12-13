9f6ce23665016d071757a03f6e7ab94b6fb05641
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-13 19:29:39 +0300

    It god damn works but I've never seed such dirty code in my live!!

--- Day 08/day08.py
+++ Day 08/day08.py
@@ -2,12 +2,14 @@ import sys
 import os
 
 os.chdir(sys.path[0])
-fileName = 'input_tst1'
+fileName = 'input'
+
 
 def findContaining(patterns: list, digit: set) -> set:
     for pattern in patterns:
         if digit.issubset(pattern):
             return pattern
+
 
 def findNotContaining(patterns: list, digit: set) -> set:
     for pattern in patterns:
@@ -16,17 +18,17 @@ def findNotContaining(patterns: list, digit: set) -> set:
 
 
 def findDigits(patterns: list) -> dict:
-    one = set(list(filter(lambda d: len(d)==2, patterns))[0])
-    four = set(list(filter(lambda d: len(d)==4, patterns))[0])
-    seven = set(list(filter(lambda d: len(d)==3, patterns))[0])
-    eight = set(list(filter(lambda d: len(d)==7, patterns))[0])
+    one = set(list(filter(lambda d: len(d) == 2, patterns))[0])
+    four = set(list(filter(lambda d: len(d) == 4, patterns))[0])
+    seven = set(list(filter(lambda d: len(d) == 3, patterns))[0])
+    eight = set(list(filter(lambda d: len(d) == 7, patterns))[0])
 
-    allSixLenSet = list(map( lambda s: set(s), filter(lambda d: len(d) == 6, patterns)))
+    allSixLenSet = list(map(lambda s: set(s), filter(lambda d: len(d) == 6, patterns)))
     six = findNotContaining(allSixLenSet, one)
     nine = findContaining(allSixLenSet, four)
-    zero = next(filter(lambda s: s != six and s!= nine, allSixLenSet))
+    zero = next(filter(lambda s: s != six and s != nine, allSixLenSet))
 
-    allFiveLenSet = list(map( lambda s: set(s), filter(lambda d: len(d) == 5, patterns)))
+    allFiveLenSet = list(map(lambda s: set(s), filter(lambda d: len(d) == 5, patterns)))
     three = findContaining(allFiveLenSet, seven)
     twoFive = list(filter(lambda s: s != three, allFiveLenSet))
     twoOrFive1 = twoFive[0]
@@ -38,25 +40,33 @@ def findDigits(patterns: list) -> dict:
         five = twoOrFive2
         two = twoOrFive1
 
-    print('one', one)
-    print('two', two)
-    print('three', three)
-    print('four', four)
-    print('five', five)
-    print('six', six)
-    print('seven', seven)
-    print('eight', eight)
-    print('nine', nine)
-    print('zero', zero)
+    return {"".join(sorted(list(zero))): '0',
+            "".join(sorted(list(one))): '1',
+            "".join(sorted(list(two))): '2',
+            "".join(sorted(list(three))): '3',
+            "".join(sorted(list(four))): '4',
+            "".join(sorted(list(five))): '5',
+            "".join(sorted(list(six))): '6',
+            "".join(sorted(list(seven))): '7',
+            "".join(sorted(list(eight))): '8',
+            "".join(sorted(list(nine))): '9'
+            }
 
 
 uniqDigits = 0
+totalOutput = 0
 with open(fileName, 'r') as file:
     for line in file:
         (before, after) = line.split('|')
         digits = after.split()
+        outputDigitsSets = list(map(lambda s: set(s), digits))
         uniqDigits += len(list(filter(lambda d: (len(d) > 1 and len(d) < 5) or len(d) == 7, digits)))
         patterns = before.split()
-        p2dDict = findDigits(patterns)
+        digitsDict = findDigits(patterns)
+        outputValue = ''
+        for outputDigit in outputDigitsSets:
+            outputDigitStr= "".join(sorted(list(outputDigit)))
+            outputValue += digitsDict[outputDigitStr]
+        totalOutput += int(outputValue)
 
-print(uniqDigits)
+print(totalOutput)
