b7eaee5682504fb9c9859116f7d1e79635b171bf
Vladimir Bukreev <VVBukreev@sberbank.ru>
2021-12-13 17:40:25 +0300

    FFFFFK Code was correct, I was sending wrong answer!

--- Day 07/day07.py
+++ Day 07/day07.py
@@ -1,12 +1,34 @@
 import sys
 import os
 
+def calcMinFuel(pos: int, crabPositions: list) -> int:
+    fuel = 0
+    for i in range(0, len(crabPositions)):
+        fuel += abs(crabPositions[i] - pos)
+
+    return fuel
+
+
 os.chdir(sys.path[0])
-fileName = 'input_tst'
+fileName = 'input'
 
 with open(fileName, 'r') as file:
     crabPositions = [int(c) for c in file.readline().split(',')]
 
-crabPositions.sort()
-print(crabPositions)
-print(crabPositions[int(len(crabPositions)/2)-1])
+# crabPositions.sort()
+minFuel = None
+minFuelPos = None
+for i in range(min(crabPositions), max(crabPositions)+1):
+# for i in range(crabPositions[0], crabPositions[len(crabPositions)-1]+1):
+    currMinFuel = calcMinFuel(i, crabPositions)
+    print (f'For pos "{i}" fuel = {currMinFuel}')
+    if minFuel == None or currMinFuel <= minFuel:
+        minFuel = currMinFuel
+        minFuelPos = i
+
+print('Answer: ', minFuelPos)
+print('Fuel: ', minFuel)
+
+
+# print(crabPositions)
+# print(crabPositions[int(len(crabPositions)/2)-1])
