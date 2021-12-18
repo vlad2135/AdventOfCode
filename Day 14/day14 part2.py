0f82cfd884a0749c6e3c154ac422b2f401ef192d
Vladimir Bukreev <vlad2135@yandex.ru>
2021-12-18 21:01:39 +0300

    still not working

--- Day 14/day14 part2.py
+++ Day 14/day14 part2.py
@@ -27,28 +27,31 @@ for i in range(0,len(polymer)-1):
 
 print(pairCnt)
 
-for i in range(0,1):
+for i in range(0,2):
     newPairCnt = defaultdict(int)
-    newPairCnt = deepcopy(pairCnt)
     for pair in pairCnt:
-        if not pair in rules:
+        newPairCnt[pair] = pairCnt[pair]
+
+    for pair in pairCnt:
+        if not pair in rules or pairCnt[pair]<=0:
             continue
 
-        newPair1 = pair[0]+rules[pair]
-        newPair2 = rules[pair]+pair[1]
-        newPairCnt[pair] = pairCnt[pair] - 1
-        if newPairCnt[pair] <= 0:
-            newPairCnt.pop(pair)
+        for N in range(0,pairCnt[pair]):
+            newPair1 = pair[0]+rules[pair]
+            newPair2 = rules[pair]+pair[1]
+            newPairCnt[pair] = pairCnt[pair] - 1
+            # if newPairCnt[pair] < 0:
+            #     newPairCnt[pair] = 0
 
-        if newPair1 in pairCnt:
-            newPairCnt[newPair1] = pairCnt[newPair1] + 1
-        else:
-            newPairCnt[newPair1] = 1
+            if newPair1 in pairCnt:
+                newPairCnt[newPair1] = pairCnt[newPair1] + 1
+            else:
+                newPairCnt[newPair1] = 1
 
-        if newPair2 in pairCnt:
-            newPairCnt[newPair2] = pairCnt[newPair2] + 1
-        else:
-            newPairCnt[newPair2] = 1
+            if newPair2 in pairCnt:
+                newPairCnt[newPair2] = pairCnt[newPair2] + 1
+            else:
+                newPairCnt[newPair2] = 1
 
     pairCnt = newPairCnt
         
@@ -57,9 +60,11 @@ print()
 print(pairCnt)
 charCnt = defaultdict(int)
 for pair in pairCnt:
+    if pairCnt[pair] <=0:
+        continue
     charCnt[pair[0]] = charCnt[pair[0]] + pairCnt[pair]
-    charCnt[pair[1]] = charCnt[pair[1]] + pairCnt[pair]
 
+charCnt[polymer[len(polymer)-1]] = charCnt[polymer[len(polymer)-1]] + 1
 print(charCnt)
 
 minOccur = min(charCnt.values())
