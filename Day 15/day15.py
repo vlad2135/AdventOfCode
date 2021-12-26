a080f3592aea754a98ad46e82f88a39e2ad7a7f5
Vladimir Bukreev <vlad2135@yandex.ru>
2021-12-27 00:32:24 +0300

    part 2 works on test sample

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 os.chdir(sys.path[0])
-fileName = 'input'
+fileName = 'input_tst'
 
 riskMap = []
 totalRiskMap = []
@@ -11,11 +11,36 @@ visitedMap = []
 with open(fileName, 'r') as file:
     for line in file:
         row = [int(c) for c in line.strip()]
-        riskMap.append(row)
-        totalRiskMap.append([None] * len(row))
-        visitedMap.append([False] * len(row))
+        riskMap.append(row * 5)
+        totalRiskMap.append([None] * len(row) * 5)
+        visitedMap.append([False] * len(row) * 5)
 
+origYcnt = len(riskMap)
+origXcnt = origYcnt
 
+newRiskMap = riskMap.copy()
+for i in range(1, 5):
+    for r in riskMap:
+        newRiskMap.append(r.copy())
+        totalRiskMap.append([None] * len(r))
+        visitedMap.append([False] * len(r))
+
+riskMap = newRiskMap.copy()
+
+for y in range(0, len(riskMap)):
+    for x in range(0, len(riskMap[0])):
+        xIncr = x // origXcnt
+        yIncr = y // origYcnt
+        if xIncr == 0 and yIncr == 0:
+            continue
+
+        newRisk = (riskMap[y][x] + xIncr + yIncr) % 9
+        if newRisk == 0:
+            newRisk = 9
+
+        riskMap[y][x] = newRisk
+
+# print('\n'.join([''.join([str(cell) for cell in row]) for row in riskMap]))
 x = 0
 y = 0
 totalRiskMap[y][x] = 0
@@ -56,8 +81,8 @@ while x != len(riskMap[0])-1 or y != len(riskMap)-1:
         if not visitedMap[y][x]:
             break
 
-    print('next visit queue len = ', len(nextVisitQueue))
-    print('curr x=',x,' and y=',y)
+    # print('next visit queue len = ', len(nextVisitQueue))
+    # print('curr x=',x,' and y=',y)
 
 print(totalRiskMap[len(riskMap)-1][len(riskMap[0])-1])
 # print('\n'.join([''.join([str(cell) for cell in row]) for row in visitedMap]))
