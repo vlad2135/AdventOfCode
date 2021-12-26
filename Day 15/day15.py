d7e031eb19c3bd818eb2c8cbcf23489470c00614
Vladimir Bukreev <vlad2135@yandex.ru>
2021-12-26 22:59:25 +0300

    it returns answer, but a wrong one

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -22,46 +22,41 @@ x = 0
 y = 0
 totalRiskMap[y][x] = 0
 
-def getNearestNotvisited(x,y,visitedMap):
+
+def getNearestNotvisited(x, y, visitedMap):
     nvList = []
-    for dx in range(-1,2):
-        for dy in range(-1,2):
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
             if ((dx != 0 and dy != 0) or
+                (dx == 0 and dy == 0) or
                 (y+dy < 0 or y+dy >= len(visitedMap)) or
                 (x+dx < 0 or x+dx >= len(visitedMap[0])) or
-                (visitedMap[y+dy][x+dx])):
+                    (visitedMap[y+dy][x+dx])):
                 continue
             nvList.append((x+dx, y+dy))
-
-    if len(nvList)==0:
-        breakpoint()
 
     return nvList
 
 
-while x != len(riskMap[0])-1 or y != len(riskMap)-1:
-    minRiskNextY = -1
-    minRiskNextX = -1
+nextVisitQueue = []
 
-    for (nX, nY) in getNearestNotvisited(x,y,visitedMap):
+while x != len(riskMap[0])-1 or y != len(riskMap)-1:
+    nearestNotVisited = getNearestNotvisited(x, y, visitedMap)
+    for (nX, nY) in nearestNotVisited:
         nRisk = totalRiskMap[y][x] + riskMap[nY][nX]
         if totalRiskMap[nY][nX] == None or totalRiskMap[nY][nX] > nRisk:
             totalRiskMap[nY][nX] = nRisk
-        if minRiskNextX == -1:
-            minRiskNextX = nX
-            minRiskNextY = nY
-        elif totalRiskMap[nY][nX] < totalRiskMap[minRiskNextY][minRiskNextX]:
-            minRiskNextX = nX
-            minRiskNextY = nY
-        
-    if minRiskNextX == -1:
-        breakpoint()
+
+    if len(nearestNotVisited) > 0:
+        nnvGlobalRisks = [(totalRiskMap[y][x], x, y)
+                          for (x, y) in nearestNotVisited]
+        nnvgrReverseSorted = sorted(nnvGlobalRisks, key=lambda t: t[0])
+        for t in nnvgrReverseSorted:
+            nextVisitQueue.append((t[1], t[2]))
 
     visitedMap[y][x] = True
-    x = minRiskNextX
-    y = minRiskNextY
+    (x, y) = nextVisitQueue.pop()
 
-print(totalRiskMap[9][9])
+print(totalRiskMap[len(riskMap)-1][len(riskMap[0])-1])
 # print('\n'.join([''.join([str(cell) for cell in row]) for row in visitedMap]))
 # print('\n'.join([''.join([str(cell) for cell in row]) for row in totalRiskMap]))
-
