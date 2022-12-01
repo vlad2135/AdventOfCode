b5c7cd015b7ea05201365c10cec196793a7e99ed
Vladimir Bukreev <vlad2135@yandex.ru>
2022-12-01 09:45:43 +0300

    Day 01 part 02 complete

--- AoC2022/Day 01/day01.js
+++ AoC2022/Day 01/day01.js
@@ -4,23 +4,17 @@ import * as readline from 'node:readline/promises';
 const inputStream = fs.createReadStream('AoC2022/Day 01/input.txt');
 const lineRead = readline.createInterface({ input: inputStream});
 
-let elfNumber = 1;
 let calories = 0;
-let maxCalories = calories;
-let elfWithMaxCalories = elfNumber;
+let caloriesArr = [];
 
 for await (const line of lineRead){
     if (line) {
         calories += Number(line);
     }
     else {
-        if (calories > maxCalories) {
-            elfWithMaxCalories = elfNumber
-            maxCalories = calories
-        };
+        caloriesArr.push(calories)
         calories = 0;
-        elfNumber++;
     }
 }
-console.log(`Elf # with max calories = ${elfWithMaxCalories}`);
-console.log(`He carries this much calories = ${maxCalories}`);
+caloriesArr.sort((a,b) => b -a);
+console.log(caloriesArr.slice(0,3).reduce((a,b) => a+b, 0));
