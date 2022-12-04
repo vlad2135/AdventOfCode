85c186ec38d81b8a95463e85175ec530352a21f4
Vladimir Bukreev <vlad2135@yandex.ru>
2022-12-05 01:25:28 +0300

    Wow, Day 04 part 01 complete very fast

---
+++ AoC2022/Day 04/day04-01.js
@@ -0,0 +1,18 @@
+
+import * as fs from 'node:fs';
+import * as readline from 'node:readline/promises';
+
+const inputStream = fs.createReadStream('AoC2022/Day 04/input.txt');
+const lineRead = readline.createInterface({ input: inputStream });
+
+let fullContainCount = 0;
+for await (const line of lineRead) {
+    let [range1, range2] = line.trim().split(',');
+    let [r1start, r1end] = range1.split('-').map(s => Number(s));
+    let [r2start, r2end] = range2.split('-').map(s => Number(s));
+    if ((r1start>= r2start && r1end <= r2end) || 
+        (r2start >= r1start && r2end <= r1end)) {
+        fullContainCount++;
+    }
+}
+console.log(fullContainCount);
