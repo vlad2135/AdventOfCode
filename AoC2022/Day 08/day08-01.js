e1bc7e46f985686373d8f06d83b7c7bfb520379b
Vladimir Bukreev <vlad2135@yandex.ru>
2022-12-12 01:23:14 +0300

    Day 08 part 01 finally complete :(

--- AoC2022/Day 08/day08-01.js
+++ AoC2022/Day 08/day08-01.js
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const lines = fs.readFileSync('AoC2022/Day 08/input_train.txt','utf-8').split(/\r?\n/);
+const lines = fs.readFileSync('AoC2022/Day 08/input.txt','utf-8').split(/\r?\n/);
 lines.pop(); // removing last empty line from array
 
 const trees = [];
@@ -17,24 +17,16 @@ for (let y = 0; y < sideLen; y++) {
 
     visible[y] = Array(sideLen).fill(false);
 
-    let stopOnLeft = false;
-    let stopOnRight = false;
     for (let x = 0; x < sideLen; x++) {
 
-        if (!stopOnLeft && trees[y][x] > currHrowLeft) {
+        if (trees[y][x] > currHrowLeft) {
             visible[y][x] = true;
             currHrowLeft = trees[y][x];
         }
-        else { stopOnLeft = true; }
 
-        if (!stopOnRight && trees[y][sideLen - x - 1] > currHrowRight) {
+        if (trees[y][sideLen - x - 1] > currHrowRight) {
             visible[y][sideLen - x - 1] = true;
             currHrowRight = trees[y][sideLen - x - 1];
-        }
-        else { stopOnRight = true; }
-
-        if (stopOnLeft && stopOnRight) {
-            break;
         }
     }
 }
@@ -43,33 +35,25 @@ for (let x = 0; x < sideLen; x++) {
     let currHrowTop = -1;
     let currHrowBottom = -1;
 
-    let stopOnTop = false;
-    let stopOnBottom = false;
     for (let y = 0; y < sideLen; y++) {
 
-        if (!stopOnTop && trees[y][x] > currHrowTop) {
+        if (trees[y][x] > currHrowTop) {
             visible[y][x] = true;
             currHrowTop = trees[y][x];
         }
-        else { stopOnTop = true; }
 
-        if (!stopOnBottom && trees[sideLen - y - 1][x] > currHrowBottom) {
+        if (trees[sideLen - y - 1][x] > currHrowBottom) {
             visible[sideLen - y - 1][x] = true;
             currHrowBottom = trees[sideLen - y - 1][x];
-        }
-        else { stopOnBottom = true; }
-
-        if (stopOnTop && stopOnBottom) {
-            break;
         }
     }
 }
 // console.log(visible.reduce((a,v) => a + v.reduce((ia, iv) => ia + iv ? 1: 0, 0), 0));
 let totalVis = 0;
 for (let y = 0; y < sideLen; y++) {
-    console.log(visible[y].map(b => b == true ? '1': '0').join(''));
+    // console.log(visible[y].map(b => b == true ? '1': '0').join(''));
     let currVis = visible[y].reduce((a,v) => a + (v ? 1: 0), 0);
-    console.log(currVis);
+    // console.log(currVis);
     totalVis += currVis;
 }
 console.log(totalVis);
