f95c855216d30ecd0e2ef1d2690c20f1e85b3088
Vladimir Bukreev <vlad2135@yandex.ru>
2023-12-09 13:11:32 +0300

    Day 04 part 01 done

--- AoC2023/CSharp/day04/Program.cs
+++ AoC2023/CSharp/day04/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines(@"..\..\..\input4.txt");
+
+long points = 0;
+
+foreach (var line in lines) {
+    var p = line.Split(':', StringSplitOptions.TrimEntries); 
+
+    var numbers = p[1].Split('|', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+    var winNums = numbers[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList();
+
+    var myNums = numbers[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    List<int> sortedMyNums = myNums.Select(Int32.Parse).ToList();
+    sortedMyNums.Sort();
+
+    int winCount = 0;
+    foreach (int winNum in winNums) {
+        if (sortedMyNums.BinarySearch(winNum) >= 0) {
+            winCount++;
+        }
+    }
+
+    if (winCount > 0)
+    {
+        points += (int)Math.Pow(2, winCount - 1);
+    }
+}
+
+Console.WriteLine(points);
