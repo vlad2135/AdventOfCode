e56ca7ed735c8a54179712eba7cc9bdeb8a5e007
Vladimir Bukreev <vlad2135@yandex.ru>
2024-12-01 19:41:15 -0500

    Input data + solution for part 1

--- AoC2024/CSharp/day01/Program.cs
+++ AoC2024/CSharp/day01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = await File.ReadAllLinesAsync(@"..\..\..\input.txt");
+var list1 = new List<int>();
+var list2 = new List<int>();
+foreach (var line in lines)
+{
+    var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    list1.Add(Int32.Parse(parts[0]));
+    list2.Add(Int32.Parse(parts[1]));
+}
+list1.Sort();
+list2.Sort();
+
+int dist = 0;
+
+for (int i = 0; i < list1.Count; i++)
+{
+    dist += Math.Max(list1[i], list2[i]) - Math.Min(list1[i], list2[i]);
+}
+Console.WriteLine(dist);
