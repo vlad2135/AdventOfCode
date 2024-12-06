9a68adcee148e60a7ee33e294a714bb344b081ca
Vladimir Bukreev <vlad2135@yandex.ru>
2024-12-05 23:05:32 -0500

    OH MY GOD THEY'VE TRICKED ME

--- AoC2024/CSharp/day03/ProgramDay03.cs
+++ AoC2024/CSharp/day03/ProgramDay03.cs
@@ -1,31 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.RegularExpressions;
 
-// var lines = await File.ReadAllLinesAsync(@"..\..\..\input.txt");
+var lines = await File.ReadAllLinesAsync(@"..\..\..\input.txt");
 // string[] lines = [@"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
-string[] lines = [@"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)do()+mul(32,64](mul(11,8)undon't()?mul(8,5))do()_mul(5,5)+"];
+// string[] lines = [@"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"];
 
 Regex mulRx = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled);
 Regex dontDoRx = new Regex(@"don't\(\).+?(do\(\)|$)", RegexOptions.Compiled);
 
 long sum = 0;
 
-// foreach (var line in lines)
+File.Delete(@"..\..\..\input_cut.txt");
+string line = "";
+
 for (int i = 0; i < lines.Length; i++)
 {
-    string line = lines[i];
-    line = dontDoRx.Replace(line, "");
+    line += lines[i];
+}
+    line = dontDoRx.Replace(line, "!!!");
+    File.AppendAllLines(@"..\..\..\input_cut.txt", [line]);
 
     var match = mulRx.Match(line); 
 
     while (match.Success)
     {
-        int m1 = Int32.Parse(match.Groups[1].Value);
-        int m2 = Int32.Parse(match.Groups[2].Value);
-        sum += m1 * m2;
+        Console.WriteLine(match.Groups[0].Value);
+        long m1 = Int64.Parse(match.Groups[1].Value);
+        long m2 = Int64.Parse(match.Groups[2].Value);
+        sum = sum + (m1 * m2);
 
         match = match.NextMatch();
     }
-}
 
 Console.WriteLine(sum);
