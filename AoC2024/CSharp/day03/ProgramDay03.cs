4ae07435dec90f51c24df9fb5dbd0cd17eac5d8f
Vladimir Bukreev <vlad2135@yandex.ru>
2024-12-05 22:02:44 -0500

    2nd part of day 3 doesn't work but it should

--- AoC2024/CSharp/day03/ProgramDay03.cs
+++ AoC2024/CSharp/day03/ProgramDay03.cs
@@ -1,14 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.RegularExpressions;
 
-var lines = await File.ReadAllLinesAsync(@"..\..\..\input.txt");
+// var lines = await File.ReadAllLinesAsync(@"..\..\..\input.txt");
 // string[] lines = [@"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
+string[] lines = [@"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)do()+mul(32,64](mul(11,8)undon't()?mul(8,5))do()_mul(5,5)+"];
+
 Regex mulRx = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled);
+Regex dontDoRx = new Regex(@"don't\(\).+?(do\(\)|$)", RegexOptions.Compiled);
 
 long sum = 0;
 
-foreach (var line in lines)
+// foreach (var line in lines)
+for (int i = 0; i < lines.Length; i++)
 {
+    string line = lines[i];
+    line = dontDoRx.Replace(line, "");
+
     var match = mulRx.Match(line); 
 
     while (match.Success)
