ad3cc380f7c30e0a77cee8218afe4807f0468f8f
Vladimir Bukreev <vlad2135@yandex.ru>
2024-12-08 00:51:03 -0500

    part 1 of day 05 is done

--- AoC2024/CSharp/day05/ProgramDay05.cs
+++ AoC2024/CSharp/day05/ProgramDay05.cs
@@ -1,2 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var lines = await File.ReadAllLinesAsync(@"..\..\..\input.txt");
+bool is1stPart = true;
+Dictionary<int, (HashSet<int> before, HashSet<int> after)> rules = new Dictionary<int, (HashSet<int>, HashSet<int>)>();
+int addedMiddleNumbers = 0;
+// await foreach (var line in File.ReadLinesAsync(@"..\..\..\input_test.txt"))
+await foreach (var line in File.ReadLinesAsync(@"..\..\..\input.txt"))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        is1stPart = false;
+        continue;
+    }
+
+    if (is1stPart) {
+        var values = line.Split("|").Select(Int32.Parse);
+        AddToRules(values.First(), values.Last());
+    }
+    else
+    {
+        addedMiddleNumbers += GetMiddleOfCorrectUpdate(line.Split(",").Select(Int32.Parse).ToList());
+    }
+
+}
+Console.WriteLine(addedMiddleNumbers);
+
+void AddToRules(int before, int after)
+{
+    if (!rules.TryGetValue(before, out var _))
+    {
+        rules[before] = new (new HashSet<int>(), new HashSet<int>());
+    }
+    rules[before].after.Add(after);
+
+    if (!rules.TryGetValue(after, out var _))
+    {
+        rules[after] = new (new HashSet<int>(), new HashSet<int>());
+    }
+    rules[after].before.Add(before);
+}
+
+int GetMiddleOfCorrectUpdate(List<int> update)
+{
+    bool isRightOrder = true;
+    for (int i = 0; i < update.Count; i++)
+    {
+        if (rules[update[i]].before.Intersect(update.GetRange(i, update.Count - i)).Any())
+        {
+            isRightOrder = false;
+            break;
+        }
+        if (rules[update[i]].after.Intersect(update.GetRange(0, i)).Any())
+        {
+            isRightOrder = false;
+            break;
+        }
+    }
+    return isRightOrder ? update[update.Count / 2] : 0;
+}
